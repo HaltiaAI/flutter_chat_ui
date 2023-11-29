e3e8da4ece4862ddefc2f08d8d4279c04016faf0
Serhii Miskiv <serjjedi@gmail.com>
2023-11-30 01:33:09 +0200

    WIP: fix for not exporting new file

--- lib/flutter_chat_ui.dart
+++ lib/flutter_chat_ui.dart
@@ -19,6 +19,7 @@ export 'src/widgets/input/input_text_field_controller.dart';
 export 'src/widgets/input/send_button.dart';
 export 'src/widgets/message/file_message.dart';
 export 'src/widgets/message/image_message.dart';
+export 'src/widgets/message/markdown_message.dart';
 export 'src/widgets/message/message.dart';
 export 'src/widgets/message/message_status.dart';
 export 'src/widgets/message/system_message.dart';
