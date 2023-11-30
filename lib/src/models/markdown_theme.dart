021128da1874e5861af549f469fd4fc86d9290b2
Serhii Miskiv <serhii.miskiv@Serhiis-MacBook-Pro.local>
2023-11-30 03:52:54 +0200

    WIP: markdown theme added

---
+++ lib/src/models/markdown_theme.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+
+abstract class MarkdownTheme {
+  TextStyle get a;
+  TextStyle get p;
+  TextStyle get code;
+  TextStyle get blockquote;
+  BoxDecoration get blockquoteDecoration;
+  EdgeInsetsGeometry get blockquotePadding;
+  TextStyle get h1;
+  TextStyle get h2;
+  TextStyle get h3;
+  TextStyle get h4;
+  TextStyle get listBullet;
+  double get listIndent;
+
+  MarkdownStyleSheet toMarkdownStyleSheet();
+}
--- lib/src/widgets/message/markdown_message.dart
+++ lib/src/widgets/message/markdown_message.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_markdown/flutter_markdown.dart';
 
+import '../../models/markdown_theme.dart';
 import '../state/inherited_chat_theme.dart';
 import 'user_name.dart';
 
@@ -9,57 +10,15 @@ class MarkdownTextMessage extends StatelessWidget {
   const MarkdownTextMessage({
     super.key,
     required this.message,
-    this.nameBuilder,
     required this.showName,
+    required this.markdownTheme,
+    this.nameBuilder,
   });
 
   final types.TextMessage message;
+  final MarkdownTheme markdownTheme;
   final Widget Function(types.User)? nameBuilder;
   final bool showName;
-
-  MarkdownStyleSheet _customMarkdownStyleSheet(BuildContext context) =>
-      MarkdownStyleSheet(
-        a: const TextStyle(
-          color: Colors.blue,
-          fontWeight: FontWeight.bold,
-        ), // Links
-        p: const TextStyle(fontSize: 16.0, color: Colors.black87), // Paragraphs
-        code: TextStyle(
-          backgroundColor: Colors.grey[200],
-          fontFamily: 'monospace',
-          fontSize: 14.0,
-        ), // Inline code.
-        blockquote: TextStyle(color: Colors.grey[600]), // Blockquotes
-        blockquoteDecoration: BoxDecoration(
-          color: Colors.grey[100],
-          border:
-              Border(left: BorderSide(color: Colors.grey[300]!, width: 4.0)),
-        ),
-        blockquotePadding: const EdgeInsets.all(10.0),
-        h1: const TextStyle(
-          fontSize: 24.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.deepOrange,
-        ), // Heading 1
-        h2: const TextStyle(
-          fontSize: 22.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.orange,
-        ), // Heading 2
-        h3: const TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.blueAccent,
-        ), // Heading 3
-        h4: const TextStyle(
-          fontSize: 18.0,
-          fontWeight: FontWeight.bold,
-        ), // Heading 4
-        listBullet:
-            const TextStyle(fontSize: 16.0, color: Colors.red), // Bullet lists
-        listIndent: 24.0, // Indentation for lists
-        // Add other styles as needed...
-      );
 
   @override
   Widget build(BuildContext context) {
@@ -78,8 +37,7 @@ class MarkdownTextMessage extends StatelessWidget {
                 UserName(author: message.author),
           MarkdownBody(
             data: message.text,
-            // TODO: We probably need to set up whole new color & style theme
-            styleSheet: _customMarkdownStyleSheet(context),
+            styleSheet: markdownTheme.toMarkdownStyleSheet(),
           ),
         ],
       ),
