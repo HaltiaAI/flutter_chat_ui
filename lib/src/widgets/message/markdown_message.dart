fb56d880cf2e232af991339702658a8af125198e
Serhii Miskiv <serhii.miskiv@Serhiis-MacBook-Pro.local>
2023-12-10 22:14:51 +0200

    WIP: changes

--- lib/src/widgets/message/markdown_message.dart
+++ lib/src/widgets/message/markdown_message.dart
@@ -3,31 +3,25 @@ import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_markdown/flutter_markdown.dart';
 
 import '../state/inherited_chat_theme.dart';
-import '../state/inherited_user.dart';
 import 'user_name.dart';
 
 class MarkdownTextMessage extends StatelessWidget {
   const MarkdownTextMessage({
     super.key,
     required this.message,
-    required this.codeStyle,
+    required this.markdownStyleSheet,
     required this.showName,
     this.nameBuilder,
   });
 
   final types.TextMessage message;
-  final TextStyle codeStyle;
+  final MarkdownStyleSheet markdownStyleSheet;
   final Widget Function(types.User)? nameBuilder;
   final bool showName;
 
   @override
   Widget build(BuildContext context) {
     final theme = InheritedChatTheme.of(context).theme;
-
-    final user = InheritedUser.of(context).user;
-    final bodyStyle = user.id == message.author.id
-        ? theme.sentMessageBodyTextStyle
-        : theme.receivedMessageBodyTextStyle;
 
     return Container(
       margin: EdgeInsets.symmetric(
@@ -42,10 +36,7 @@ class MarkdownTextMessage extends StatelessWidget {
                 UserName(author: message.author),
           MarkdownBody(
             data: message.text,
-            styleSheet: MarkdownStyleSheet(
-              p: bodyStyle,
-              code: codeStyle,
-            ),
+            styleSheet: markdownStyleSheet,
           ),
         ],
       ),
